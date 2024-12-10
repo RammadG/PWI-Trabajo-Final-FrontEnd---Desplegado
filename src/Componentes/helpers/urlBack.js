9586cf7a721352607a05526a932af467eee19d79
ramma.dG <ramadegregori@gmail.com>
2024-12-10 19:17:53 -0300

    ...

--- src/Componentes/helpers/urlBack.js
+++ src/Componentes/helpers/urlBack.js
@@ -1,4 +1,4 @@
-//const URL_BACK = 'https://trabajo-final-backend-pwf-desplegado.vercel.app'
-const URL_BACK = 'http://localhost:8000'
+const URL_BACK = 'https://trabajo-final-backend-pwf-desplegado.vercel.app'
+//const URL_BACK = 'http://localhost:8000'
 
 export default URL_BACK
